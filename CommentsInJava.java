81e4f11a2bfefb543118a874e8290725165ae27e
Harshada Kadam <harshadakadamjob@gmail.com>
2025-10-02 14:37:39 +0530

    Comments in java Program

---
+++ CommentsInJava.java
@@ -0,0 +1,15 @@
+public class CommentsInJava {
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+        System.out.println("Hello World");
+
+        /*
+         * This is a multi-line comment
+         * It can span over multiple lines
+         * Java will ignore all of this
+         */
+        System.out.println("Hello World");
+
+    }
+}
