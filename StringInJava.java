32452b0896efdd83e3fe13ce8c89414723129065
Harshada Kadam <harshadakadamjob@gmail.com>
2025-10-03 12:57:08 +0530

    Added String constructor in Java that creates a new string from a part of a character array  in java

--- StringInJava.java
+++ StringInJava.java
@@ -11,6 +11,20 @@ public class StringInJava {
         // String Literals ("Hello") → Stored in String Constant Pool. If the same
         // literal already exists, Java reuses the reference.
         System.out.println(a1 == a2); // true
+
+        // it is a String constructor in Java that creates a new string from a part of a
+        // character array.
+
+        /*
+         * Parameters:
+         * 
+         * 1) value → the source character array
+         * 2) offset → starting index in the array
+         * 3) count → number of characters to copy
+         */
+        char[] arr = { 'H', 'A', 'R', 'S', 'H', 'A', 'D', 'A' };
+        String newarr = new String(arr, 0, 4);
+        System.out.println(newarr);
     }
 }
 
